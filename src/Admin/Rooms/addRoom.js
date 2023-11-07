92d90bb74acf5066b81caa4b0c789e592cb4d65b
malleswaritippireddy28 <146261592+malleswaritippireddy28@users.noreply.github.com>
2023-11-07 16:58:55 -0600

    ref-5

--- src/Admin/Rooms/addRoom.js
+++ src/Admin/Rooms/addRoom.js
@@ -2,16 +2,10 @@ import React, { useEffect, useRef, useState } from "react";
 import Grid from "@mui/material/Grid";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
 import Box from "@mui/material/Box";
 import LockOutlinedIcon from "@mui/icons-material/Home";
 import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { Link, useNavigate } from "react-router-dom";
 import { addRoom, getLocation, mapToken } from "../../utilities/utility";
 import mapboxgl from "mapbox-gl";
 
