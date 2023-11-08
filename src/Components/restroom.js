a22efa0a0d4d2288e59575fb6d9da37e0b1eb95a
malleswaritippireddy28 <146261592+malleswaritippireddy28@users.noreply.github.com>
2023-11-07 18:21:15 -0600

    bug-2-added_delet_room

--- src/Components/restroom.js
+++ src/Components/restroom.js
@@ -104,11 +104,6 @@ export default function RestRoom({ room, isMobile, ...props }) {
           </CardActions>
         </Box>
       </Box>
-      {getToken()?.isAdmin && (
-        <Button size="small" onClick={() => {}}>
-          <Delete onClick={onDeleteRoom} />
-        </Button>
-      )}
     </Card>
   );
 }
